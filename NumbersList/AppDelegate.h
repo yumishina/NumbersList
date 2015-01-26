47e4c79fccbb7e10277a9ab185414fde7475adb0
yumishina <yu-mishina@yandex.ru>
2015-01-26 07:37:36 +0300

    Initial commit

---
+++ NumbersList/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  NumbersList
+//
+//  Created by Юлия on 26.01.15.
+//  Copyright (c) 2015 YuliaMishina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ NumbersList/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NumbersList
+//
+//  Created by Юлия on 26.01.15.
+//  Copyright (c) 2015 YuliaMishina. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
