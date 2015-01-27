7f8a8f33068ba1c2b621b5662a23051489a2d58d
yumishina <yu-mishina@yandex.ru>
2015-01-27 17:11:39 +0300

    Заполнение tableView

--- NumbersList/ViewController.h
+++ NumbersList/ViewController.h
@@ -8,7 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@property(nonatomic,strong) NSArray* arrayNumbers;
+
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
 
 
 @end
--- NumbersList/ViewController.m
+++ NumbersList/ViewController.m
@@ -16,8 +16,27 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.tableView.delegate = self;
+    self.tableView.dataSource =  self;
+     [self.navigationController.navigationBar setTranslucent:NO]; // отключаем свойство полупрозрачности панели навигации
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"customCell"];
     // Do any additional setup after loading the view, typically from a nib.
+    self.arrayNumbers = @[@"1", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"10", @"11", @"12", @"13", @"14", @"15", @"16", @"17", @"18", @"19", @"20", @"21"];
 }
+
+//Количество строк tableView
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.arrayNumbers.count;
+}
+
+//Заполнение данными tableView
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"customCell" forIndexPath:indexPath];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone; //удаление выделения ячейки
+    cell.textLabel.text = [self.arrayNumbers objectAtIndex:indexPath.row];
+    return cell;
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
